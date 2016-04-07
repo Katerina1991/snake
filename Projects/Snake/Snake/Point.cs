d2e90ecc13643936c222a0e2b45314a58c7d8737
Katerina1991 <Katerina.iove@yandex.ru>
2016-04-07 21:52:00 +0400

    classes

---
+++ Projects/Snake/Snake/Point.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        int x; int y; char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public int X
+        {
+            get {return x;}
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public char Sym
+        {
+            get { return sym; }
+            set { sym = value; }
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+    }
+}
