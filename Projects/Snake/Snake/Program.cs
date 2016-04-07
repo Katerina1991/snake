5096a717b2bff4e32c0f8881a09e33fd826a81c4
Katerina1991 <Katerina.iove@yandex.ru>
2016-04-07 23:04:33 +0400

    lines

--- Projects/Snake/Snake/Program.cs
+++ Projects/Snake/Snake/Program.cs
@@ -10,12 +10,25 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(80, 25);
+
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-         
+
+            HorisontalLine UpLine = new HorisontalLine(0, 78, 0, '+');
+            UpLine.Draw();
+            HorisontalLine DownLine = new HorisontalLine(0, 78, 24, '+');
+            DownLine.Draw();
+
+            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
+            leftLine.Draw();
+
+            VerticalLine rightLine = new VerticalLine(78, 0, 24, '+');
+            rightLine.Draw();
+
             Console.ReadLine();
         }
 
